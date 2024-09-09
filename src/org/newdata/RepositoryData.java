0dbb52a8a5c78eb6950d2e7cf35eee7f93d2e8de
sathiyaboss <sathiyaboss50@gmail.com>
2024-09-09 19:16:13 +0530

    conflict resolved

--- src/org/newdata/RepositoryData.java
+++ src/org/newdata/RepositoryData.java
@@ -4,13 +4,14 @@ public class RepositoryData {
 	
 	public static void main(String[] args) {
 		System.out.println("sucess");
+		System.out.println("hello");
 		
-<<<<<<< HEAD
 		int a = 10;
 		System.out.println(a);
-=======
-		System.out.println("hello");
->>>>>>> 9ef293feedec23cb19b3394ae1fe1691ec7c3dae
+		
+		
+		
+
 	}
 
 }
